ea2db5d86f25a2d5b935465319b57915f14074cc
MURAHARI RAJU <115579740+MurahariRaju@users.noreply.github.com>
2023-04-16 21:56:52 +0530

    Add files via upload
    color-picker code

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function selectedButton1(){
+    var ChoosenColor= document.getElementById("button1").innerHTML;
+    document.body.style.backgroundColor=ChoosenColor;
+    document.getElementById("selectedColorHexCode").textContent=ChoosenColor;
+    }
+
+
+function selectedButton2(){
+
+    var ChoosenColor= document.getElementById("button2").innerHTML;
+    document.body.style.backgroundColor=ChoosenColor;
+    document.getElementById("selectedColorHexCode").textContent=ChoosenColor;
+}
+function selectedButton3(){
+
+    var ChoosenColor= document.getElementById("button3").innerHTML;
+    document.body.style.backgroundColor=ChoosenColor;
+    document.getElementById("selectedColorHexCode").textContent=ChoosenColor;
+}
+
+function selectedButton4(){
+
+    var ChoosenColor= document.getElementById("button4").innerHTML;
+    document.body.style.backgroundColor=ChoosenColor;
+    document.getElementById("selectedColorHexCode").textContent=ChoosenColor;
+}
+
+
+
